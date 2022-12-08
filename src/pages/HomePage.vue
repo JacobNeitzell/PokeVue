d39246f8aeb98ef5066aea8459cee8bbd0a51eb3
Jacob Neitzell <107662321+JacobNeitzell@users.noreply.github.com>
2022-12-08 12:53:47 -0700

    Fixing it up

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,27 +3,27 @@
     <div class="row">
       <div class="col-3">
         <ul>
-          <li v-for="d in digimons" :key="d.name">
-            <button @click="getActivePokemon(d.url)" class="btn btn-outline-danger my-1">
-              {{d.name}}
+          <li v-for="p in pokemons" :key="p.name">
+            <button @click="getActivePokemon(p.url)" class="btn btn-outline-danger my-1">
+              {{ p.name }}
             </button>
           </li>
         </ul>
         <button @click="changePage(previousPage)" :disabled="!previousPage" class="btn btn-danger me-2"
-          :class="{'disabled' : !previousPage}">Previous</button>
+          :class="{ 'disabled': !previousPage }">Previous</button>
         <button @click="changePage(nextPage)" :disabled="!nextPage"
           :class="`btn btn-danger ${!nextPage ? 'btn-info' : ''}`">Next</button>
       </div>
       <!-- NOTE will not break page when trying to dig into a null property  use v-if or elvis (?) -->
-      <div v-if="digimon" class="col-9">
-        <img :src="digimon?.img" alt="" class="img-fluid">
-        <div>{{digimon?.name}}</div>
+      <div v-if="pokemon" class="col-9">
+        <img :src="pokemon?.img" alt="" class="img-fluid">
+        <div>{{ pokemon?.name }}</div>
         <div class="d-flex gap-3">
-          <img v-for="t in digimon?.types" :key="t"
+          <img v-for="t in pokemon?.types" :key="t"
             :src="`https://jakeoverall.github.io/who-is-that-pokemon/assets/img/types/${t.type?.name}.webp`" alt=""
             height="64" :title="t.type?.name">
         </div>
-        <img v-for="s in digimon?.sprites" :src="s" alt="">
+        <img v-for="s in pokemon?.sprites" :src="s" alt="">
       </div>
     </div>
   </div>
@@ -49,9 +49,9 @@ export default {
     getPokemons()
     return {
       // NOTE this is bad, don't do this
-      digimons: computed(() => AppState.pokemons),
+      pokemons: computed(() => AppState.pokemons),
       // NOTE this is bad, don't do this
-      digimon: computed(() => AppState.pokemon),
+      pokemon: computed(() => AppState.pokemon),
       nextPage: computed(() => AppState.nextPage),
       previousPage: computed(() => AppState.previousPage),
       async changePage(pageUrl) {
