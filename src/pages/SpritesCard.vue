17a08cebbd5e074b1820e946ad6ad4e526800ec2
Jacob Neitzell <107662321+JacobNeitzell@users.noreply.github.com>
2022-12-13 14:26:08 -0700

    Adjusting the pokecard

---
+++ src/pages/SpritesCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="component">
+    <div class="Keep-Card p-1">
+      <div class="card text-dark " data-bs-target="#keeps-modal" data-bs-toggle="modal">
+
+        <img class="card-img" alt="Card-Image" />
+        <div class="card-img-overlay">
+          <h5 class="card-title text-shadow">{{ keep.name }}</h5>
+          <p class="card-text"></p>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    return {
+
+
+
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
